210cc3e48fd146aafba639a321811ee485f8e2ae
Robert Kirkman <misternumberone@live.com>
2024-06-11 05:55:34 -0500

    assignment 1 solution

---
+++ finder-app/writer.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Script for assignment 1
+# Author: Robert Kirkman
+
+if [[ -z "$1" || -z "$2" ]]
+then
+    echo "usage: $0 [writefile] [writestr]"
+    exit 1
+fi
+
+if ! install -D <(echo $2) $1
+then
+    echo "failed to write \"$2\" to $1"
+    exit 1
+fi
--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,13 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    // TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+
+    const char *username_statically_allocated = my_username();
+    char *username_dynamically_allocated = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(username_statically_allocated,
+                                     username_dynamically_allocated,
+                                     "The strings username_statically_allocated "
+                                     "and username_dynamically_allocated are not equal.");
+    free(username_dynamically_allocated);
 }
